241d357e14cc17cdd1ed5e3d7f9e142526d70c74
xWafl <73sampleperson@gmail.com>
2020-06-29 10:53:52 -0400

    :bug: Fixed assorted bugs

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
-import { Bot, matchPrefixes } from "@enitoni/gears";
-import { Adapter, CommandGroup, Command } from "@enitoni/gears-discordjs";
+import { matchPrefixes } from "@enitoni/gears";
+import { Bot, Adapter, CommandGroup, Command } from "@enitoni/gears-discordjs";
 
 const adapter = new Adapter({
-    token: process.env.BOT_TOKEN
+    token: process.env.BOT_TOKEN!
 });
 
-const command = new Command({
-    matcher: matchPrefixes("test"),
-    action: context => {
+const command = new Command()
+    .match(matchPrefixes("test"))
+    .use(context => {
 	const { message } = context;
 	message.channel.send("Test received!");
-    }
-});
+    });
 
-const group = new CommandGroup({
-    matcher: matchPrefixes("!"),
-    commands: [command]
-});
+const group = new CommandGroup()
+    .match(matchPrefixes("!"))
+    .setCommands(command);
 
-const bot = new Bot({ adapter, group });
-bot.start();
+const bot = new Bot({ adapter, commands: [group] });
+
+const main = async () => {
+    await bot.start();
+    console.log("Bot started!");
+}
+
+main();
